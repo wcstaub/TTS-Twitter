bd8f6b1bfc6ae6bcaadf961330ccd0ca7d3bb9f3
Wendy S <wstaub@fastmail.fm>
2018-02-14 08:55:22 -0500

    slides 1-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
- 
+  validates :username, presence: true, uniqueness: true
   mount_uploader :avatar, AvatarUploader
 end
 
